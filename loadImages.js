a04f850ff602ddda6968341321046b8e55c14cd2
wables <wablesphoto@gmail.com>
2024-01-23 01:38:33 -0800

    update lawbstation ipfs

--- loadImages.js
+++ loadImages.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const imageDisplay = document.getElementById('imageDisplay');
     const enterButton = document.getElementById('enterButton');
     const clearButton = document.getElementById('clearButton');
-    const baseURL = 'https://nftstorage.link/ipfs/bafybeiclv57f23hra6e6p2ucwe2bs2bvo27m62kjjxr3gglinyy6hvs7xq';
 
     enterButton.addEventListener('click', async () => {
         const imageNumber = searchInput.value;
         if (imageNumber >= 0 && imageNumber < 420) {
             imageDisplay.innerHTML = ''; // Clear current display before showing new image
             try {
-                const response = await fetch(baseURL + imageNumber + '.json');
+                const ipfsGateway = 'https://nftstorage.link/ipfs/';
+                const response = await fetch(`${ipfsGateway}bafybeie26yz7lz53dg2yykfzkv5jd4stgswm7xb4q46s7sftwo7beglxme/${imageNumber}.json`);
                 const data = await response.json();
                 const img = document.createElement('img');
-                img.src = baseURL + data.image;
+                img.src = data.image.startsWith('ipfs://') ? data.image.replace('ipfs://', ipfsGateway) : data.image;
                 img.alt = data.name;
                 img.style.width = 'calc(50% - 10px)'; // Adjust size as needed
                 img.style.height = 'auto';
