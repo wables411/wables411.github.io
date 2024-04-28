f7ce98e7648eebc8db341c704e38ebe0e3b12c20
wables <wablesphoto@gmail.com>
2024-04-28 14:28:52 -0700

    nexustrans

--- loadimages3.js
+++ loadimages3.js
@@ -6,21 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     enterButton.addEventListener('click', async () => {
         const imageNumber = searchInput.value;
-        if (imageNumber >= 0 && imageNumber <= 999) {  // Keeps the same range
-            imageDisplay.innerHTML = ''; // Clear current display
+        if (imageNumber >= 0 && imageNumber < 999) {
+            imageDisplay.innerHTML = ''; // Clear current display before showing new image
             try {
-                const imageBasePath = 'images/isolatednexus/';  // Adjust to match the server route to your folder
-                const response = await fetch(`${imageBasePath}${imageNumber}.json`);
+                const ipfsGateway = 'https://bafybeic6uouxkzrdj2cv4f7ssrelf6t732jwket253mc746xrlv4csml3u.ipfs.cf-ipfs.com/';
+                const response = await fetch(`${ipfsGateway}${imageNumber}.json`);
                 const data = await response.json();
                 const img = document.createElement('img');
-                img.src = `${imageBasePath}${imageNumber}.png`;
+                img.src = `${ipfsGateway}${imageNumber}.png`; // Use the number to refer to the image directly
                 img.alt = data.name;
-                img.style.width = '50%'; // Keeps the same image styling
+                img.style.width = 'calc(50% - 10px)'; // Adjust size as needed
                 img.style.height = 'auto';
                 imageDisplay.appendChild(img);
             } catch (error) {
                 console.error('Error loading image:', error);
-                imageDisplay.innerText = 'Failed to load image. Please check the console for more details.';
+                imageDisplay.innerText = 'Failed to load image.';
             }
         } else {
             imageDisplay.innerText = 'Invalid image number. Please enter a number between 0 and 999.';
@@ -28,7 +28,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     clearButton.addEventListener('click', () => {
-        imageDisplay.innerHTML = '';
-        searchInput.value = '';
+        imageDisplay.innerHTML = ''; // Clear the image display
+        searchInput.value = ''; // Clear the search input
     });
 });
+
