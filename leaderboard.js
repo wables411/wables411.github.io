864205f55703bc626fcb0bcb180ca3cd1e14553c
wables <141197729+wables411@users.noreply.github.com>
2024-11-05 10:38:31 -0800

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -3,7 +3,7 @@ const supabaseUrl = 'https://roxwocgknkiqnsgiojgz.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJveHdvY2drbmtpcW5zZ2lvamd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3NjMxMTIsImV4cCI6MjA0NjMzOTExMn0.NbLMZom-gk7XYGdV4MtXYcgR8R1s8xthrIQ0hpQfx9Y';
 
 // Using global Supabase from CDN
-const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey, {
+const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
         persistSession: false
     }
