546a56d94c86ba07c66b51d1d315762686c93443
wables <141197729+wables411@users.noreply.github.com>
2024-11-05 11:15:05 -0800

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,5 +1,5 @@
 // Initialize Supabase client with CDN version
-const supabase = supabase.createClient(
+const supabase = window.supabase.createClient(
     'https://roxwocgknkiqnsgiojgz.supabase.co',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJveHdvY2drbmtpcW5zZ2lvamd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3NjMxMTIsImV4cCI6MjA0NjMzOTExMn0.NbLMZom-gk7XYGdV4MtXYcgR8R1s8xthrIQ0hpQfx9Y'
 );
