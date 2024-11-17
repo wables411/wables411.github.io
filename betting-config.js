543132d71ef4e34a27beccb2cc6cb52397ab5039
Runitupronnie <runitupronnie@gmail.com>
2024-11-17 13:10:58 -0800

    Fix chessBetting.js

--- betting-config.js
+++ betting-config.js
@@ -1,15 +1,12 @@
 // betting-config.js
 const BETTING_CONFIG = {
     // Network configuration
-    NETWORK: 'https://solana-mainnet.rpc.extrnode.com/218119a6-454e-430e-b63c-f1ae113c7eed',
+    NETWORK: 'https://solana-mainnet.rpc.extrnode.com',
     CONNECTION_CONFIG: {
         commitment: 'confirmed',
         confirmTransactionInitialTimeout: 60000
     },
 
-    // Wallet and contract addresses
-    HOUSE_WALLET: '3NCvL5itgJVrwNZw8BNL8syP8Za5hAmhmApCDh4bdsTu',
-    
     // LAWB Token configuration
     LAWB_TOKEN: {
         MINT: '65GVcFcSqQcaMNeBkYcen4ozeT83tr13CeDLU4sUUdV6',
@@ -20,13 +17,10 @@ const BETTING_CONFIG = {
     // Betting parameters
     HOUSE_FEE_PERCENTAGE: 5,
     MIN_BET: 100,
-    MAX_BET: 5000000,
-    
-    // Game timeouts (in seconds)
-    MATCH_TIMEOUT: 30 * 60,
-    BET_CONFIRMATION_TIMEOUT: 2 * 60,
+    MAX_BET: 100000,
     
     // Escrow configuration
+    HOUSE_WALLET: '3NCvL5itgJVrwNZw8BNL8syP8Za5hAmhmApCDh4bdsTu', // Your house wallet address
     ESCROW_SEED: 'chess_escrow'
 };
 
