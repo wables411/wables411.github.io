f8c5767c81dd062482a9f632b5a487308104a436
Runitupronnie <runitupronnie@gmail.com>
2024-11-19 19:40:15 -0800

    Fix chessBetting.js

--- solana-config.js
+++ solana-config.js
@@ -2,29 +2,25 @@ window.SOLANA_CONFIG = {
     CLUSTER: 'mainnet-beta',
     ENDPOINTS: {
         'mainnet-beta': [
-            'https://rpc.ankr.com/solana',  // Move Ankr to first position
-            'https://solana-api.projectserum.com',
-            'https://solana-mainnet.rpc.extrnode.com/218119a6-454e-430e-b63c-f1ae113c7eed'
+            'https://rpc.ankr.com/solana',  // Use Ankr as primary
+            'https://solana-api.projectserum.com'  // Backup
         ]
     },
     CONNECTION_CONFIG: {
         commitment: 'confirmed',
         confirmTransactionInitialTimeout: 60000,
-        httpHeaders: {
-            'Authorization': 'Bearer 218119a6-454e-430e-b63c-f1ae113c7eed',
-            'Content-Type': 'application/json'
-        },
-        wsEndpoint: 'wss://solana-mainnet.rpc.extrnode.com/218119a6-454e-430e-b63c-f1ae113c7eed'
-    },
-    PROGRAM_IDS: {
-        TOKEN: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
-        ASSOCIATED_TOKEN: 'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
-        SYSTEM: '11111111111111111111111111111111'
+        disableRetryOnRateLimit: false,
+        wsEndpoint: false  // Disable websocket
     },
     async createConnection() {
         for (const endpoint of this.ENDPOINTS[this.CLUSTER]) {
             try {
-                const connection = new solanaWeb3.Connection(endpoint, this.CONNECTION_CONFIG);
+                const connection = new solanaWeb3.Connection(endpoint, {
+                    ...this.CONNECTION_CONFIG,
+                    httpHeaders: {
+                        'Content-Type': 'application/json'
+                    }
+                });
                 // Test connection
                 await connection.getSlot();
                 console.log('Connected to Solana network:', endpoint);
