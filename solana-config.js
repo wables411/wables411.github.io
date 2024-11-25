7c2c5ba6112791faec811734d98f1178836a3ad2
Runitupronnie <runitupronnie@gmail.com>
2024-11-25 11:52:52 -0800

    Fix solana-config.js

--- solana-config.js
+++ solana-config.js
@@ -3,16 +3,13 @@ window.SOLANA_CONFIG = {
     CLUSTER: 'mainnet-beta',
     ENDPOINTS: {
         'mainnet-beta': [
-            'https://solana-mainnet.rpc.extrnode.com'
+            'https://proud-cool-butterfly.solana-mainnet.quiknode.pro/af788f06eb1d204aa3b010a2df6a3d97bb3c1b0f/'
         ]
     },
     CONNECTION_CONFIG: {
         commitment: 'confirmed',
         confirmTransactionInitialTimeout: 60000,
         httpHeaders: {
-            'Authorization': 'Bearer 218119a6-454e-430e-b63c-f1ae113c7eed',
-            'Origin': 'https://lawb.xyz',
-            'Referer': 'https://lawb.xyz',
             'Content-Type': 'application/json'
         }
     },
@@ -20,37 +17,20 @@ window.SOLANA_CONFIG = {
     async createConnection() {
         const endpoint = this.ENDPOINTS[this.CLUSTER][0];
         try {
-            console.log('Creating Extrnode connection...');
-            
-            // Create pre-configured fetch function
-            const fetchFunction = (url, options) => {
-                const finalOptions = {
-                    ...options,
-                    headers: {
-                        ...options.headers,
-                        ...this.CONNECTION_CONFIG.httpHeaders
-                    }
-                };
-                return fetch(url, finalOptions);
-            };
+            console.log('Creating QuickNode connection...');
             
             const connection = new solanaWeb3.Connection(
                 endpoint, 
-                {
-                    commitment: this.CONNECTION_CONFIG.commitment,
-                    confirmTransactionInitialTimeout: this.CONNECTION_CONFIG.confirmTransactionInitialTimeout,
-                    httpHeaders: this.CONNECTION_CONFIG.httpHeaders,
-                    fetch: fetchFunction
-                }
+                this.CONNECTION_CONFIG
             );
             
             // Test connection
             await connection.getVersion();
-            console.log('Extrnode connection established');
+            console.log('QuickNode connection established');
             return connection;
             
         } catch (error) {
-            console.error('Extrnode connection failed:', error);
+            console.error('QuickNode connection failed:', error);
             throw error;
         }
     }
