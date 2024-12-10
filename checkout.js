7190dbab8afc0388be5f85b2ac6d8a54354901e7
Runitupronnie <runitupronnie@gmail.com>
2024-12-10 13:49:49 -0800

    shop.html

--- checkout.js
+++ checkout.js
@@ -1,5 +1,3 @@
-const { createClient } = supabase;
-
 const supabase = createClient(
     'https://roxwocgknkiqnsgiojgz.supabase.co',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJveHdvY2drbmtpcW5zZ2lvamd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3NjMxMTIsImV4cCI6MjA0NjMzOTExMn0.NbLMZom-gk7XYGdV4MtXYcgR8R1s8xthrIQ0hpQfx9Y'
